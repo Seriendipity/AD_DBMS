1a20cd6213e38ab77b382bb6dea4c9ad05ce916e
tomacx <139841470+tomacx@users.noreply.github.com>
2024-03-19 21:45:28 +0800

    This is CHENXIHONG’s modification

--- src/test/java/SqlParser/SqlParserTest.java
+++ src/test/java/SqlParser/SqlParserTest.java
@@ -4,5 +4,6 @@ public class SqlParserTest {
     public static void main(String[] args) {
         System.out.println("This is a test for the sql");
         System.out.println("This is CHENXIHONG's modification");
+        System.out.println("Hello,World! Congratulations!");
     }
 }
