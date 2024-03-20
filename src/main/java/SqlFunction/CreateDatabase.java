a3f7037159fd77b2e10a53fce1b6163a913eb5f5
tomacx <139841470+tomacx@users.noreply.github.com>
2024-03-20 19:50:49 +0800

    增加一个创建数据库的类SqlFunction文件中，CreateDatabase类

--- src/main/java/SqlFunction/CreateDatabase.java
+++ src/main/java/SqlFunction/CreateDatabase.java
@@ -2,14 +2,12 @@ package SqlFunction;
 import java.io.*;
 public class CreateDatabase {
     public CreateDatabase(String DatabaseName){
-        try{
-            //用户创建数据库目录
-            File file = new File("./MyDatabase/"+DatabaseName+"");
-            file.createNewFile();
-            System.out.println("数据库"+DatabaseName+"创建完成！");
-        }catch(IOException e){
-            //打印异常信息
-            e.printStackTrace();
+        //用户创建数据库目录
+        File file = new File("./MyDatabase/"+DatabaseName+"");
+        if(file.mkdir() == true) {
+            System.out.println("数据库" + DatabaseName + "创建完成！");
+        }else{
+            System.out.println("数据库" + DatabaseName + "创建失败！");
         }
     }
 }
