63e559a6291c9fb280204512a953110d30e2c6cf
tomacx <139841470+tomacx@users.noreply.github.com>
2024-03-21 23:04:16 +0800

    增加删除数据库中表格功能

---
+++ src/main/java/SqlFunction/DropTable.java
@@ -0,0 +1,20 @@
+package SqlFunction;
+
+import java.io.*;
+
+public class DropTable {
+    public DropTable(String DatabaseName,String TableName){
+        boolean flag = false;
+        File file = new File("./MyDatabase/"+DatabaseName+TableName+"");
+        if(file.exists()){
+            flag = file.delete();
+            if(!flag){
+                System.out.println("表格"+TableName+"删除失败！");
+            }else{
+                System.out.println("表格"+TableName+"删除成功！");
+            }
+        }else{
+            System.out.println("表格"+TableName+"不存在！");
+        }
+    }
+}
--- src/test/java/SqlParser/SqlParserTest.java
+++ src/test/java/SqlParser/SqlParserTest.java
@@ -8,12 +8,10 @@ public class SqlParserTest {
         //测试创建数据库
 //        CreateDatabase database = new CreateDatabase("demo1");
 //        CreateTable table = new CreateTable("demo1","table1");
-
-     //   CreateDatabase database = new CreateDatabase("demo2");
-        CreateTable table = new CreateTable("demo2","table1");
-        DropDatabase dropDatabase = new DropDatabase("demo2");
+        //测试删除数据库
+//        CreateDatabase database = new CreateDatabase("demo2");
+//        CreateTable table = new CreateTable("demo2","table1");
+//        DropDatabase dropDatabase = new DropDatabase("demo2");
         System.out.println("This is a test for the sql");
-        System.out.println("This is CHENXIHONG's modification");
-        System.out.println("Hello,World! Congratulations!");
     }
 }
